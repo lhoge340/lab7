21f436a1cb1b65876fa8e42fcdde3ab334824db5
Lyndee Hoge <lyndee.hoge@ou.edu>
2019-03-07 09:30:35 -0600

    Initial commit with given files and initial task list

---
+++ Todo.java
@@ -0,0 +1,25 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-07
+OU ID_______________: 113442594
+OU EMAIL____________: lynden.hoge@ou.edu
+
+Tasks:
+1. Write StackHospital<PatientType>
+2. Write QueueHospital<PatientType>
+3. Write PriorityQueueHospital<PatientType>
+4. Write SickPerson
+5. Write HealthyPerson
+6. Test StackHospital<PatientType>
+7. Test QueueHospital<PatientType>
+8. Test PriorityQueueHospital<PatientType>
+9. Test SickPerson
+10. Test HealthyPerson
+
+Estimated Time (minutes):
+1. 
+
+Actual Time (minutes):
+1. 
+
+*/}
